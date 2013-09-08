a040d6cba73e8f3b4a31a9706e7669bff6587aea
yangpeng <yangpeng@denong.com>
2013-09-08 10:33:34 +0800

    重写Override方法

--- src/com/denong/HelloWorld.java
+++ src/com/denong/HelloWorld.java
@@ -10,4 +10,30 @@ public class HelloWorld {
 	public void setStr(String str) {
 		this.str = str;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((str == null) ? 0 : str.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		HelloWorld other = (HelloWorld) obj;
+		if (str == null) {
+			if (other.str != null)
+				return false;
+		} else if (!str.equals(other.str))
+			return false;
+		return true;
+	}
+	
 }
---
+++ src/com/denong/MyClass.java
@@ -0,0 +1,10 @@
+package com.denong;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
