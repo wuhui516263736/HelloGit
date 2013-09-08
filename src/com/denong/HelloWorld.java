3bf1ab4c88a1dd356710400da5d461d8ab166a22
yangpeng <yangpeng@denong.com>
2013-09-08 08:33:20 +0800

    增加属性

--- src/com/denong/HelloWorld.java
+++ src/com/denong/HelloWorld.java
@@ -1,5 +1,13 @@
 package com.denong;
 
 public class HelloWorld {
+	private String str;
 
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
 }
